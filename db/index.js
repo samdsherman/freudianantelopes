8ed2bb7569e370bd6cec7346de6a958295d7a1f3
clarkdowner <cddowner@gmail.com>
2016-10-25 08:36:24 -0700

    Update database schema with join table and to reflect naming conventions

--- db/index.js
+++ db/index.js
@@ -11,41 +11,35 @@ var dbConnection = mysql.createConnection({
 });
 
 
+// @ Will - I think this logic is repetitive given our shema.sql document
+// exports.Users = db.define('users', {
+// 	// table schema
+//   id: {type: Sequelize.INTEGER, primaryKey: true},
+//   username: Sequelize.STRING,
+//   password: Sequelize.STRING
+// }, {
+// 	timestamps: false
+// });
 
-exports.Users = db.define('users', {
-  id: {type: Sequelize.INTEGER, primaryKey: true},
-  username: Sequelize.STRING,
-  password: Sequelize.STRING
-	// table schema
-}, {
-	timestamps: false
-}
-);
+// exports.Groups = db.define('groups', {
+// 	// table schema
+// 	id: {type: Sequelize.INTEGER, primaryKey: true},
+// 	user_id: Sequelize.INTEGER,
+// 	name: Sequelize.STRING
+// }, {
+// 	timestamps: false
+// });
 
-exports.Groups = db.define('groups', {
-	id: {type: Sequelize.INTEGER, primaryKey: true},
-	userId: Sequelize.INTEGER,
-	name: Sequelize.STRING
-
-	// table schema
-}, {
-	timestamps: false
-}
-);
-
-exports.Members = db.define('members', {
-	// table schema
-	id: {type: Sequelize.INTEGER, primaryKey: true},
-	name: Sequelize.STRING,
-	facebook: Sequelize.STRING,
-	instagram: Sequelize.STRING,
-	twitter: Sequelize.STRING
-}, {
-	timestamps: false
-}
-);
-
-
+// exports.Members = db.define('members', {
+// 	// table schema
+// 	id: {type: Sequelize.INTEGER, primaryKey: true},
+// 	name: Sequelize.STRING,
+// 	facebook: Sequelize.STRING,
+// 	instagram: Sequelize.STRING,
+// 	twitter: Sequelize.STRING
+// }, {
+// 	timestamps: false
+// });
 
 
 module.exports.dbConnection = dbConnection;
