5e7a173d86c0bdaafe588064e973a80b8f5cf1db
Ker Moua <kermoua92@gmail.com>
2016-10-26 10:13:52 -0700

    Change to state component to handle group creation

--- client/sidebar.jsx
+++ client/sidebar.jsx
@@ -1,10 +1,29 @@
-var Sidebar = props => (
-  <div className='sidebar'>
-    <div className='sidebar-header'>Your groups:</div>
-    <div className='sidebar-groups'>
-      {props.groups.map(groupName => <Group name={groupName} clickHandler={props.groupClickHandler} />)}
-    </div>
-  </div>
-);
+class Sidebar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			createGroupClicked: false
+		};
+	}
+
+	openCreateGroupForm() {
+		console.log('openCreateGroupForm called');
+		this.setState({createGroupClicked: !this.state.createGroupClicked});
+	}
+
+	render(props) {
+		return (
+		  <div className='sidebar'>
+		 
+		  	<CreateGroupLink createGroupClicked={this.state.createGroupClicked} openCreateGroupForm={this.openCreateGroupForm.bind(this)}/>
+	
+		    <div className='sidebar-groups-header'>Your groups:</div>
+		    <div className='sidebar-groups'>
+		      {this.props.groups.map(groupName => <Group name={groupName} clickHandler={this.props.groupClickHandler} />)}
+		    </div>
+		  </div>
+		);
+	}
+}
 
 window.Sidebar = Sidebar;
