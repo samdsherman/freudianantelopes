4934077a6e4995d874e1fd95cd226d9bda29eb68
Ker Moua <kermoua92@gmail.com>
2016-10-26 22:41:57 -0700

    Change to state component to handle rendering of edit form

--- client/group.jsx
+++ client/group.jsx
@@ -1,8 +1,40 @@
-var Group = props => (
-  <div className='group'>
-    <span className='group-name' onClick={() => props.clickHandler(props.name)}>{props.name}</span>
-    <button className='group-edit'>Edit...</button>
-  </div>
-);
+class Group extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			editGroupForm: null,
+			editGroupClicked: false
+		};
+	}	
+	
+	handleEditClick(e) {
+		e.preventDefault();
+
+		// if a click would change this.state.editGroupClicked to true,
+		// we know a form should be generated after the click so we set the new state
+		if (!this.state.editGroupClicked) {
+			this.setState({
+				editGroupForm: <EditGroup handleEditClick={this.handleEditClick.bind(this)}/>,
+				editGroupClicked: true
+			})
+		} else {
+			// if clicked again, the form would hides from view
+			this.setState({
+				editGroupForm: null,
+				editGroupClicked: false
+			})
+		}
+	}
+
+	render() {
+		return (
+	  <div className='group'>
+	    <span className='group-name' onClick={() => this.props.clickHandler(this.props.name)}>{this.props.name}</span>
+	    <a href='#' className='group-edit' onClick={this.handleEditClick.bind(this)}>Edit...</a>
+	    {this.state.editGroupForm}
+	  </div>
+		);
+	}
+}
 
 window.Group = Group;
